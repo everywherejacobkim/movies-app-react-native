72cda69287d95baabcefae6b07d0b8effa9ae6c2
everywherejacobkim <everywherejacobkim@gmail.com>
2022-10-01 00:31:45 -0700

    Build: create header component

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import Stack from './src/navigation/Stack';
+import Header from './src/components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Header />
+        {/* <Stack />   */}
+      </NavigationContainer>
+    </NativeBaseProvider>
+
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box, HStack, StatusBar, Text } from 'native-base'
+
+const Header = () => (
+    <>
+        <StatusBar barStyle='light-content'/>
+            <Box safeAreaTop backgroundColor='#1d3557'>
+                <HStack bg='#1d3557' px={1} py={2} alignItems='center' justifyContent='center'>
+                    <Text color='#fff' fontSize={20} fontWeight={600}>Movies App</Text>
+                </HStack>
+            </Box>    
+        <Text>Header</Text>
+    </>
+)
+
+export default Header
